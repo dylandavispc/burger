1b0866059fe56788be8112ab1c561eb9de681800
Dylan Davis <dylandavispc@gmail.com>
2019-11-02 11:32:23 -0400

    heroku hero

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Dilldill1",
-    database: "krusty"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Dilldill1",
+        database: "krusty"
+    });
+}
 
 
 
